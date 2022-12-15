15149fb3382c28fc937050ea8188e06089ea1fb2
Luis Silva <luansimo@outlook.com>
2022-12-15 01:45:02 -0600

    Something

--- index.js
+++
@@ -1,7 +0,0 @@
-const { readFile, readFileSync } = require ('fs');
-
-const txt = readFileSync('./hello.txt', 'utf-8');
-
-readFile('./hello.txt', 'utf-8', (err, txt) => {console.log(txt)} )
-
-console.log('do this asap')
